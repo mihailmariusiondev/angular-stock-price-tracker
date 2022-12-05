d12a6c29a755b4ef7c3ca029ce6bbee076345f09
Marius <marius.mihail@enzo.es>
2022-12-05 18:46:05 +0100

    step 2 finished

--- src/app/modules/stock-tracker/components/stock-tracker/stock-tracker.component.ts
+++ src/app/modules/stock-tracker/components/stock-tracker/stock-tracker.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, Observable, take, tap } from 'rxjs';
 import { company } from '../../models/company';
 import { quote } from '../../models/quote';
 import { StockTrackerService } from '../services/stock-tracker.service';
@@ -15,13 +15,15 @@ type companyAndQuote = company & quote;
 export class StockTrackerComponent implements OnInit {
 
   // Our stock tracker form
-  stockForm = new FormGroup({
-    symbol: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(5), this.duplicateSymbolValidator]),
-  });
+  stockForm: FormGroup;
+  companyStockCombined: companyAndQuote[] = [];
+  localStorageKey = 'companiesAndQuotes';
 
-  companyStockCombined: companyAndQuote[] = []
-
-  constructor(private stockTrackerService: StockTrackerService) {}
+  constructor(private fb: FormBuilder, private stockTrackerService: StockTrackerService) {
+    this.stockForm = fb.group({
+      symbol: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(5), this.duplicateSymbolValidator, this.noWhiteSpaceValidator]]
+    });
+  }
 
   ngOnInit() {
     this.getSymbols();
@@ -29,46 +31,58 @@ export class StockTrackerComponent implements OnInit {
 
   // Load the symbols from local storage, if they exist
   getSymbols() {
-    let localStorageSymbols = localStorage.getItem('companiesAndQuotes');
+    let localStorageSymbols = localStorage.getItem(this.localStorageKey);
     if (localStorageSymbols) {
       this.companyStockCombined = JSON.parse(localStorageSymbols) as companyAndQuote[];
     }
   }
 
   addSymbol() {
-    const stockSymbol = this.stockForm.value.symbol?.toUpperCase()
+    const stockSymbol = this.stockForm.value.symbol?.toUpperCase();
+    console.log(stockSymbol)
     if (!stockSymbol) return;
 
-    let company$ = this.stockTrackerService.getCompanyName(stockSymbol)
-    let quote$ = this.stockTrackerService.getQuote(stockSymbol)
+    let company$ = this.stockTrackerService.getCompanyName(stockSymbol).pipe(take(1));
+    let quote$ = this.stockTrackerService.getQuote(stockSymbol).pipe(take(1));
 
-    // Combine the latest values emitted by quote$ and company$
-    combineLatest([company$, quote$]).subscribe(([quote, company]) => {
-      const companyAndQuote: companyAndQuote = Object.assign({}, quote, company);
-
-      // Push the new symbol to the list of symbols and save it to local storage
-      this.companyStockCombined = [...this.companyStockCombined, companyAndQuote]
-      localStorage.setItem('companiesAndQuotes', JSON.stringify(this.companyStockCombined));
-
-      this.stockForm.reset();
-    });
+    combineLatest([company$, quote$])
+      .pipe(
+      tap(([quote, company]) => {
+        const companyAndQuote : companyAndQuote = {...quote, ...company};
+        this.companyStockCombined = [...this.companyStockCombined, companyAndQuote];
+      }),
+      )
+      .subscribe(() => {
+        localStorage.setItem(this.localStorageKey, JSON.stringify(this.companyStockCombined));
+        this.stockForm.reset();
+      });
   }
 
+
   removeSymbol(symbol: string) {
-    this.companyStockCombined = this.companyStockCombined.filter(companyAndQuote => companyAndQuote.symbol !== symbol);
-    localStorage.setItem('companiesAndQuotes', JSON.stringify(this.companyStockCombined));
+    const index = this.companyStockCombined.findIndex(companyAndQuote => companyAndQuote.symbol === symbol);
+    if (index >= 0) {
+      this.companyStockCombined.splice(index, 1);
+      localStorage.setItem('companiesAndQuotes', JSON.stringify(this.companyStockCombined));
+    }
   }
 
   // Check if the symbol is already in local storage
   duplicateSymbolValidator(control: FormControl) {
-    if(!control.value) return null
+    if (!control.value) return null;
     let localStorageSymbols = localStorage.getItem('companiesAndQuotes');
     let symbols: companyAndQuote[] = localStorageSymbols ? JSON.parse(localStorageSymbols) : [];
 
     // Check if the symbol is already in the companiesAndQuotes array
-    if (symbols.some(s => s.symbol === control.value.toUpperCase())) {
+    if (symbols.find(s => s.symbol === control.value.toUpperCase())) {
       return { duplicate: true };
     }
     return null;
   }
+
+  public noWhiteSpaceValidator(control: FormControl) {
+    const isSpace = (control.value || '').match(/\s/g);
+    return isSpace ? {whitespace: true} : null;
+}
+
 }
